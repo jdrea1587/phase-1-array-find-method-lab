7d641f1cff1a0b0aefe97dca22ae39733a83f94c
Josh <joshdanielrea@gmail.com>
2021-07-22 21:44:12 -0500

    commit tree cc103c6591bead93526c7847b30db5b688472e02

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+    const superbowlWin = (record) => {
+    //  console.log(record)}
+        const foundSeason = record.find(function(season){
+          return season.result === "W"
+      })
+        if(foundSeason){
+        return foundSeason.year
+      }
+    }
+  
 
+
+
+
+
+    // function superbowlWin(record){
+    //   const foundSeason = redord.find(season => season.result === "W")
+    //   record.find(winChecker)
+    // }
+
+
+//  function winChecker(record){
+     
+//       season.result === "W"
+//     }
+
+//iterates through objects in an array
+//assigns season that equals === "W" to a variable
+//if that season equals === "W" then return season year
